b8273ba1eb8483e754aa05629c06af9e18edcdf1
Viktor <vityakiselev96@gmail.com>
2021-10-12 21:37:19 +0300

    After first part of lecture №1

---
+++ Makefile
@@ -0,0 +1,5 @@
+build: fib.o
+	g++ main.cpp fib.o -o main.exe
+
+fib.o: fib.cpp
+	g++ -c fib.cpp -o fib.o
---
+++ build/CMakeFiles/fib_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fib_test.dir/fib_test.cpp.o"
+  "fib_test"
+  "fib_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fib_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
